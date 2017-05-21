362c6bc5c83682201b40339ac7ebd9b3ee117133
Никита Сиротин < >
2017-05-21 08:58:14 +0300

    some codes

--- project-files/dms/dms-app/view-models/quality analysis view models/QualityAnalysisManagerViewModel.cs
+++ project-files/dms/dms-app/view-models/quality analysis view models/QualityAnalysisManagerViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace dms.view_models.quality_analysis_view_models
 {
@@ -10,5 +12,33 @@ namespace dms.view_models.quality_analysis_view_models
     {
 
         public event Action<QualityAnalysisManagerViewModel> requestShowQualityAnalysis;
+    }
+
+
+    public class CreateTableQualityViewModel : ViewmodelBase
+    {
+        private ObservableCollection<DataGridColumn> _columnCollection = new ObservableCollection<DataGridColumn>();
+        public ObservableCollection<DataGridColumn> ColumnCollection
+        {
+            get
+            {
+                return this._columnCollection;
+            }
+            set
+            {
+                _columnCollection = value;
+                NotifyPropertyChanged("ColumnCollection");
+            }
+        }
+        public CreateTableQualityViewModel()
+        {
+            ColumnCollection.Add(
+                new DataGridTextColumn()
+                {
+                    Header = "Column1"
+                });
+        }
+
+        public string[] Tasks { get; set; }
     }
 }
